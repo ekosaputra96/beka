1c897330f260407f40ea539ddc4dcf11425672d2
Eko Saputra <ekosaputra1331@gmail.com>
2025-01-13 23:45:40 +0700

    progress: model migration controller teachers

--- database/migrations/2025_01_13_163750_create_teachers_table.php
+++ database/migrations/2025_01_13_163750_create_teachers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStudentsTable extends Migration
+class CreateTeachersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('teachers', function (Blueprint $table) {
             $table->id();
             $table->string('nama_depan');
             $table->string('nama_belakang');
             $table->string('tempat_lahir', 100);
             $table->date('tanggal_lahir');
-            $table->string('nis', 15)->unique()->nullable();
+            $table->string('nip', 15)->unique()->nullable();
             $table->enum('jenis_kelamin', ['m', 'f']);
             $table->auditable();
             $table->timestamps();
@@ -33,6 +33,6 @@ class CreateStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('teachers');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::middleware(['auth'])->prefix('admin')->group(function(){
     Route::resource('students', 'StudentController');
+    Route::resource('teachers', 'TeacherController');
 });
